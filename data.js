e7f70595a89cb395c37e8b54160de398cca550e7
StratosGL <stratosglavass@yahoo.com>
2025-08-31 21:28:56 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,47 @@
+export const tweetsData = [   
+    {
+        handle: `@AfroSamurai 🗡️`,
+        profilePic: `images/afro-samurai-1.jpg`,
+        likes: 1200,
+        retweets: 300,
+        tweetText: `The path of vengeance never ends. Only blood stains the number one headband.`,
+        replies: [
+            {
+                handle: `@NinjaNinja 😂`,
+                profilePic: `images/Ninja_Ninja.webp`,
+                tweetText: `Man, Afro… you ever think about chillin’ out? 🍃`
+            }
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: 'afro-uuid-1',
+    },    
+    {
+        handle: `@Justice 🎩`,
+        profilePic: `images/justice.png`,
+        likes: 800,
+        retweets: 150,
+        tweetText: `Still wearin’ that headband, boy? Don’t forget who took your father’s.`,
+        replies: [
+            {
+                handle: `@Kuma 🐻`,
+                profilePic: `images/kuma.jpg`,
+                tweetText: `Justice… your time’s coming.`
+            }
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: 'justice-uuid-2',
+    },
+    {
+        handle: `@Kuma 🐻`,
+        profilePic: `images/kuma.jpg`,
+        likes: 500,
+        retweets: 100,
+        tweetText: `The bear mask hides the pain, but revenge drives me forward.`,
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: 'kuma-uuid-3',
+    }
+]
---
+++ index.js
@@ -0,0 +1,205 @@
+import { tweetsData as defaultTweetsData } from './data.js'
+// Enable to clear local storage on page refresh
+// localStorage.removeItem("tweetsData") 
+let tweetsData = JSON.parse(localStorage.getItem("tweetsData")) || defaultTweetsData
+
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
+
+document.addEventListener('click', function (e) {
+    if (e.target.dataset.like) {
+        handleLikeClick(e.target.dataset.like)
+    }
+    else if (e.target.dataset.retweet) {
+        handleRetweetClick(e.target.dataset.retweet)
+    }
+    else if (e.target.dataset.reply) {
+        handleReplyClick(e.target.dataset.reply)
+    }
+    else if (e.target.id === 'tweet-btn') {
+        handleTweetBtnClick()
+    }
+    else if (e.target.dataset.delete) {
+        handleDeleteClick(e.target.dataset.delete);
+    }
+     else if (e.target.dataset.replyBtn) {
+        handleReplyBtnClick(e.target.dataset.replyBtn);
+    }
+})
+
+function saveToLocalStorage() {
+    localStorage.setItem("tweetsData", JSON.stringify(tweetsData));
+}
+
+function handleLikeClick(tweetId) {
+    const targetTweetObj = tweetsData.filter(function (tweet) {
+        return tweet.uuid === tweetId
+    })[0]
+
+    if (targetTweetObj.isLiked) {
+        targetTweetObj.likes--
+    }
+    else {
+        targetTweetObj.likes++
+    }
+    targetTweetObj.isLiked = !targetTweetObj.isLiked
+    saveToLocalStorage()
+    render()
+}
+
+function handleRetweetClick(tweetId) {
+    const targetTweetObj = tweetsData.filter(function (tweet) {
+        return tweet.uuid === tweetId
+    })[0]
+
+    if (targetTweetObj.isRetweeted) {
+        targetTweetObj.retweets--
+    }
+    else {
+        targetTweetObj.retweets++
+    }
+    targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted
+    saveToLocalStorage()
+    render()
+}
+
+function handleReplyClick(replyId) {
+    document.getElementById(`replies-${replyId}`).classList.toggle('hidden')
+}
+
+function handleDeleteClick(tweetId) {
+    const updatedTweets = tweetsData.filter(function (tweet) {
+        return tweet.uuid !== tweetId
+    })
+    tweetsData = updatedTweets
+    render()
+}
+
+function handleReplyBtnClick(tweetId) {
+    const replyInput = document.getElementById(`reply-input-${tweetId}`)
+
+    if (replyInput.value) {
+        const targetTweetObj = tweetsData.filter(tweet => tweet.uuid === tweetId)[0]
+
+        targetTweetObj.replies.push({
+            handle: "@youngAfro",
+            profilePic: "images/youngAfro.png",
+            tweetText: replyInput.value
+        })
+
+        render()
+
+        document.getElementById(`replies-${tweetId}`).classList.remove('hidden')
+    }
+}
+
+function handleTweetBtnClick() {
+    const tweetInput = document.getElementById('tweet-input')
+
+    if (tweetInput.value) {
+        tweetsData.unshift({
+            handle: `@youngAfro`,
+            profilePic: `images/youngAfro.png`,
+            likes: 0,
+            retweets: 0,
+            tweetText: tweetInput.value,
+            replies: [],
+            isLiked: false,
+            isRetweeted: false,
+            uuid: uuidv4()
+        })
+        saveToLocalStorage()
+        render()
+        tweetInput.value = ''
+    }
+
+}
+
+function getFeedHtml() {
+    let feedHtml = ``
+
+    tweetsData.forEach(function (tweet) {
+
+        let likeIconClass = ''
+
+        if (tweet.isLiked) {
+            likeIconClass = 'liked'
+        }
+
+        let retweetIconClass = ''
+
+        if (tweet.isRetweeted) {
+            retweetIconClass = 'retweeted'
+        }
+
+        let repliesHtml = ''
+
+        if (tweet.replies.length > 0) {
+            tweet.replies.forEach(function (reply) {
+                repliesHtml += `
+                    <div class="tweet-reply">
+                        <div class="tweet-inner">
+                            <img src="${reply.profilePic}" class="profile-pic">
+                            <div>
+                                <p class="handle">${reply.handle}</p>
+                                <p class="tweet-text">${reply.tweetText}</p>
+                            </div>
+                        </div>
+                    </div>
+                    `
+            })
+        }
+
+
+        feedHtml += `
+            <div class="tweet">
+                <div class="tweet-inner">
+                    <img src="${tweet.profilePic}" class="profile-pic">
+                    <div>
+                        <p class="handle">${tweet.handle}</p>
+                        <p class="tweet-text">${tweet.tweetText}</p>
+                        <div class="tweet-details">
+                            <span class="tweet-detail">
+                                <i class="fa-regular fa-comment-dots"
+                                data-reply="${tweet.uuid}"
+                                ></i>
+                                ${tweet.replies.length}
+                            </span>
+                            <span class="tweet-detail">
+                                <i class="fa-solid fa-heart ${likeIconClass}"
+                                data-like="${tweet.uuid}"
+                                ></i>
+                                ${tweet.likes}
+                            </span>
+                            <span class="tweet-detail">
+                                <i class="fa-solid fa-retweet ${retweetIconClass}"
+                                data-retweet="${tweet.uuid}"
+                                ></i>
+                                ${tweet.retweets}
+                            </span>
+                            <span class="tweet-detail">
+                                <i class="fa-solid fa-trash"
+                                data-delete="${tweet.uuid}"
+                                ></i>
+                            </span>
+                        </div>   
+                    </div>            
+                </div>
+                <div class="hidden" id="replies-${tweet.uuid}">
+                    ${repliesHtml}
+                    <div class="reply-input-area">
+                        <input type="text" id="reply-input-${tweet.uuid}" placeholder="Write a reply..." />
+                        <button data-reply-btn="${tweet.uuid}">Reply</button>
+                    </div>
+                </div>
+            </div>
+            `
+    })
+    return feedHtml
+}
+
+function render() {
+    document.getElementById('feed').innerHTML = getFeedHtml()
+}
+
+render()
+
